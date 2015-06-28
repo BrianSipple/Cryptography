aab245d20d02638aee807dc4fe85399c8059add7
Mike Luby <mLuby@users.noreply.github.com>
2015-06-28 19:48:39 -0400

    add symmetric key crypto specs

--- symmetricKeyExchange.js
+++ symmetricKeyExchange.js
@@ -3,47 +3,50 @@ var Sender = function(keyLimit){
   this._privateInteger = Math.floor(Math.random() * keyLimit);
   this.partialKey = null;
   this._secretKey = null;
-}
-
+};
 
 Sender.prototype.generatePartialKey = function(prime, base){
   this.partialKey = Math.pow(base, this._privateInteger) % prime;
-}
+};
 
-Sender.prototype.generateSecretKey = function(partialKey){
+Sender.prototype.generateSecretKey = function(prime, partialKey){
   this._secretKey = Math.pow(partialKey, this._privateInteger) % prime;
-}
+};
 
 Sender.prototype.encryptMessage = function(plaintext){
-  return plaintext ^ this._secretKey;
-}
-
-Sender.prototype.decryptMessage = function(ciphertext){
-  return ciphertext ^ this._secretKey;
-}
-
-Sender.prototype.sendMessage = function(plaintext, recipient){
   var ciphertext = '';
   for(var i = 0; i < plaintext.length; i++){
     ciphertext += String.fromCharCode(this._secretKey ^ plaintext.charCodeAt(i));
   }
-  recipient.receiveMessage(ciphertext);
-}
+  return ciphertext;
+};
 
-Sender.prototype.receiveMessage = function(ciphertext){
+Sender.prototype.decryptMessage = function(ciphertext){
   var plaintext = '';
   for(var i = 0; i < ciphertext.length; i++){
     plaintext += String.fromCharCode(this._secretKey ^ ciphertext.charCodeAt(i));
   }
   console.log(plaintext)
   return plaintext;
-}
+};
+
+Sender.prototype.sendMessage = function(plaintext, recipient){
+  var ciphertext = this.encryptMessage(plaintext);
+  console.log('Sender.sendMessage ciphertext', ciphertext);
+  recipient.receiveMessage(ciphertext);
+  return ciphertext;
+};
+
+Sender.prototype.receiveMessage = function(ciphertext){
+  var plaintext = this.decryptMessage(ciphertext);
+  return plaintext;
+};
 
 var exchangeKeys = function(alice, bob){
-  prime = 23;
-  base = 5;
+  var prime = 23;
+  var base = 5;
   alice.generatePartialKey(prime, base);
   bob.generatePartialKey(prime, base);
-  alice.generateSecretKey(bob.partialKey);
-  bob.generateSecretKey(alice.partialKey)
-}
+  alice.generateSecretKey(prime, bob.partialKey);
+  bob.generateSecretKey(prime, alice.partialKey)
+};
